9a10a33b7df60503f620d734124e6ed055325d49
young43 <young43@kookmin.ac.kr>
2021-02-01 22:20:10 +0900

    gps data to kml files

--- GPS/src/get_gps.py
+++ GPS/src/get_gps.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-
-import roslib
 import sys
+import time
+import datetime
+import simplekml
+
+
 import rospy
 from sensor_msgs.msg import NavSatFix
 
+# MODE==1) create kml files
+# MODE==2) just show data
+
+kml = simplekml.Kml()
+save_path = '/home/young/catkin_ws/src/ISCC_Study/GPS/kml/kml_' + datetime.datetime.now().strftime("%Y%m%d%H%M%S") + ".kml"
 
 gps_data = None
+prev_time = None
 
 def gps_callback(data):
-    global  gps_data
-    gps_data = (data.latitude, data.longitude)  # 위도, 경도
+    global gps_data
+    global prev_time
 
+    gps_data = (data.latitude, data.longitude)  # 위도, 경도
+    prev_time = time.time()
 
 def start():
+    global kml
+    global prev_time
     rospy.init_node("getGps")
     rospy.Subscriber("/gps_front/fix", NavSatFix, gps_callback)
 
@@ -23,10 +36,16 @@ def start():
         if gps_data == None:
             continue
 
+        if prev_time != None and time.time()-prev_time > 2:
+            break
 
         print(gps_data)
+        latitude, longitude = gps_data
+        kml.newpoint(name="(lat/long)", description="lt/longt", coords=[(longitude, latitude)])
+        time.sleep(0.5)
 
-
+    kml.save(save_path)
+    print('-------- Save KML files({}) --------'.format(save_path.split('/')[-1]))
 
 if __name__ == '__main__':
     start()
