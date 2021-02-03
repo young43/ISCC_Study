2173b2e66e22745fe02656a81a6060a0edb0882c
young43 <seo5220@kookmin.ac.kr>
2021-02-03 13:36:08 +0900

    kml files

--- GPS/src/get_gps.py
+++ GPS/src/get_gps.py
@@ -2,29 +2,40 @@
 # -*- coding:utf-8 -*-
 
 
-import roslib
-import sys
+import time
 import rospy
 from sensor_msgs.msg import NavSatFix
 
 
 gps_data = None
+prev_gps_data = None
 
 def gps_callback(data):
-    global  gps_data
+    global gps_data
+    global prev_time
+
+    prev_time = time.time()
     gps_data = (data.latitude, data.longitude)  # 위도, 경도
 
 
 def start():
+    global gps_data
     rospy.init_node("getGps")
     rospy.Subscriber("/gps_front/fix", NavSatFix, gps_callback)
 
     while not rospy.is_shutdown():
-        if gps_data == None:
-            continue
+        if gps_data == None: continue
 
+        if prev_time != None and time.time()-prev_time >= 5:
+            print('GPS get data Finished')
+            break
 
-        print(gps_data)
+        # kml data append
+
+        time.sleep(0.5)
+
+    # save kml
+    print("helloworld")
 
 
 
